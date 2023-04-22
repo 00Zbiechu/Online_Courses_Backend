3cf8da53982d859432bad2bbf13b882ea1e089d6
unknown <00Zbiewski@gmail.com>
2023-04-22 11:49:42 +0200

    Init Repo

---
+++ src/main/java/pl/courses/online_courses_backend/OnlineCoursesBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.courses.online_courses_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCoursesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCoursesBackendApplication.class, args);
+	}
+
+}
